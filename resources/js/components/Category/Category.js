70866e3c3a3aba60ca7f3909f08ee9402124a168
bwkw <na.shota.1139@gmail.com>
2021-12-02 23:00:55 +0900

    initial commit

---
+++ resources/js/components/Category/Category.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+function Category() {
+    return (
+        <div>
+            こんにちは
+        </div>
+    );
+}
+
+export default Category;
