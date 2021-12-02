b392142e125cabf1c90ce1fb074875d8de6c6390
bwkw <na.shota.1139@gmail.com>
2021-12-02 22:59:48 +0900

    [remove]不要なuse定義削除

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Scenery;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(Scenery $scenery)
+    public function index()
     {
         return view('Home.index');
     }
