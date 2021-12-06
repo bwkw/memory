cd510fbd87940f86f20aada163456a8302e2762f
bwkw <na.shota.1139@gmail.com>
2021-12-06 23:57:24 +0900

    [add]Travel画像追加

--- resources/js/components/Category/Category.js
+++ resources/js/components/Category/Category.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
 
-function Category() {
+export default function Category(props) {
     return (
-        <div>
-            こんにちは
-        </div>
+        <ImageList sx={{ width: 1200, height: 450 }}>
+            <ImageListItem>
+                <img
+                    src='https://couple-memory.s3.ap-northeast-1.amazonaws.com/top-page/travel.jpeg'
+                    alt="Travel"
+                />
+                <ImageListItemBar
+                    title="Travel"
+                />
+            </ImageListItem>
+        </ImageList>
     );
 }
-
-export default Category;
