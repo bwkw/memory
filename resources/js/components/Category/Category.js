f8ee0aa290b5ace333708ecd43563d7ceb0e9c81
bwkw <na.shota.1139@gmail.com>
2021-12-07 00:38:34 +0900

    [add]Category4つ追加

--- resources/js/components/Category/Category.js
+++ resources/js/components/Category/Category.js
@@ -6,18 +6,39 @@ import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import ListSubheader from '@mui/material/ListSubheader';
 
+const itemData = [
+    {
+        img: 'https://couple-memory.s3.ap-northeast-1.amazonaws.com/top-page/travel.jpeg',
+        title: 'Travel',
+    },
+    {
+        img: 'https://couple-memory.s3.ap-northeast-1.amazonaws.com/top-page/food.jpeg',
+        title: 'Food',
+    },
+    {
+        img: 'https://couple-memory.s3.ap-northeast-1.amazonaws.com/top-page/scenery.jpeg',
+        title: 'Scenery',
+    },
+    {
+        img: 'https://couple-memory.s3.ap-northeast-1.amazonaws.com/top-page/dating.jpeg',
+        title: 'Dating',
+    },
+]
+
 export default function Category(props) {
     return (
-        <ImageList sx={{ width: 1200, height: 450 }}>
-            <ImageListItem>
-                <img
-                    src='https://couple-memory.s3.ap-northeast-1.amazonaws.com/top-page/travel.jpeg'
-                    alt="Travel"
-                />
-                <ImageListItemBar
-                    title="Travel"
-                />
-            </ImageListItem>
+        <ImageList xs={{ width: 1200, height: 800 }}>
+            {itemData.map((item) => (
+                <ImageListItem>
+                    <img
+                        src={item.img}
+                        alt={item.title}
+                    />
+                    <ImageListItemBar
+                        title={item.title}
+                    />
+                </ImageListItem>
+             ))}
         </ImageList>
     );
 }
