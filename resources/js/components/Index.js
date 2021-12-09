0ba4fa3f0dd043feb788ca72a9c07a8a70951c57
bwkw <na.shota.1139@gmail.com>
2021-12-09 18:38:14 +0900

    [change]呼び出すコンポーネントの変更

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import Create from './create';
+import Category from './Category/Category';
 
 function Index() {
     return (
         <div>
-            <Create />
+            <Category />
         </div>
     );
 }
