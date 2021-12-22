779ca8caf9c8081b01ea25b20a12a6c2a7e468df
bwkw <na.shota.1139@gmail.com>
2021-12-22 21:56:49 +0900

    [add]travelsの一覧表示追加

--- app/Http/Controllers/TravelController.php
+++ app/Http/Controllers/TravelController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Travel;
 use Illuminate\Http\Request;
 
 class TravelController extends Controller
 {
-    //
+    public function index(Travel $travel)
+    {
+        return view('Travel/index');
+    }
+    
+    public function index_datas()
+    {
+        $travels = Travel::all();
+        return response()->json($travels, 200);
+    }
 }
