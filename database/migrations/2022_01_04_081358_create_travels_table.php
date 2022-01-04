347da2f1b5c768317b537fe62870c0051692b411
bwkw <na.shota.1139@gmail.com>
2022-01-04 17:22:35 +0900

    [update]カラム定義

--- database/migrations/2022_01_04_081358_create_travels_table.php
+++ database/migrations/2022_01_04_081358_create_travels_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTravelTable extends Migration
+class CreateTravelsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class CreateTravelTable extends Migration
      */
     public function up()
     {
-        Schema::create('travel', function (Blueprint $table) {
+        Schema::create('travels', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->string('address', 100);
+            $table->string('image_path', 300);
             $table->timestamps();
         });
     }
@@ -26,6 +29,6 @@ class CreateTravelTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('travel');
+        Schema::dropIfExists('travels');
     }
 }
