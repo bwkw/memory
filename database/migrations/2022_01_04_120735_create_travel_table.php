0e0316c763c8a85badd914a8460975e344cb4dcb
bwkw <na.shota.1139@gmail.com>
2022-01-04 21:11:13 +0900

    [fix]テーブル名を単数形に変更

--- database/migrations/2022_01_04_120735_create_travel_table.php
+++ database/migrations/2022_01_04_120735_create_travel_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTravelsTable extends Migration
+class CreateTravelTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTravelsTable extends Migration
      */
     public function up()
     {
-        Schema::create('travels', function (Blueprint $table) {
+        Schema::create('travel', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
             $table->string('address', 100);
@@ -29,6 +29,6 @@ class CreateTravelsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('travels');
+        Schema::dropIfExists('travel');
     }
 }
--- database/seeders/TravelSeeder.php
+++ database/seeders/TravelSeeder.php
@@ -14,7 +14,7 @@ class TravelSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('travels')->insert([
+        DB::table('travel')->insert([
             'name' => '下呂温泉',
             'address' => '岐阜県下呂市湯之島',
             'image_path' => 'https://couple-memory.s3.ap-northeast-1.amazonaws.com/travels/%E4%B8%8B%E5%91%82%E6%B8%A9%E6%B3%89.jpeg',
