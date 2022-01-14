020d931967f084f0d252e134431ce380a1a4e58d
bwkw <na.shota.1139@gmail.com>
2022-01-14 17:21:39 +0900

    [update]useEffectによりaxios回数制限

--- resources/js/components/Card/SwitchCard.js
+++ resources/js/components/Card/SwitchCard.js
@@ -1,39 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { Switch } from 'react-router-dom';
 import axios from 'axios';
 import BaseCard from './BaseCard';
+import { theme } from '../../theme'
+import { ThemeProvider } from '@mui/material/styles';
 
 
 function GetLaravelApi() {
 	const [datas, setDatas] = useState([]);
 	
-	axios
-	  .get('/api/travel')
-	  .then(response => {
-	      setDatas(response.data);     //バックエンドから返ってきたデータでpostsを更新する
-	      console.log(response.data);　//取得データ確認用のconsole.log()
-	  })
-	  .catch(() => {
-	      console.log('通信に失敗しました');
-	  });
-	  
-	  return datas
-}
+	useEffect(() => {
+		axios
+			.get('/api/travels')
+		  .then(response => {
+		      setDatas(response.data);     
+		      console.log(response.data);　
+		  })
+		  .catch(() => {
+		      console.log('通信に失敗しました');
+		  });
+	},[])
 
+	return datas
+}
 
 export default function LaravelApiCard() {
 	const laravelApiDatas = GetLaravelApi();
 	
 	return(
-		<div>
-			<BaseCard items={laravelApiDatas}/>
-		</div>
+		<ThemeProvider theme={theme}>
+			<BaseCard items={laravelApiDatas} />
+		</ThemeProvider>
 	);
 }
 
 if (document.getElementById('index_card')) {
 	ReactDOM.render(
-	    <LaravelApiCard />, document.getElementById('index_card')
+	  <LaravelApiCard />, document.getElementById('index_card')
 	);
 }
