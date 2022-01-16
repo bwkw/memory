8945f588cc9e92133adc224bf39e73092f7f7fb4
bwkw <na.shota.1139@gmail.com>
2022-01-16 21:45:04 +0900

    initial commit

---
+++ app/Http/Controllers/ApiSceneryController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Scenery;
+use Illuminate\Http\Request;
+
+class ApiSceneryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Scenery  $scenery
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Scenery $scenery)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Scenery  $scenery
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Scenery $scenery)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Scenery  $scenery
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Scenery $scenery)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/SceneryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Scenery;
+use Illuminate\Http\Request;
+
+class SceneryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Scenery  $scenery
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Scenery $scenery)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Scenery  $scenery
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Scenery $scenery)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Scenery  $scenery
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Scenery $scenery)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Scenery  $scenery
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Scenery $scenery)
+    {
+        //
+    }
+}
