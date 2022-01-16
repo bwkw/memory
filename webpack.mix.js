5aab4b77ae96ac3dd41007c135ac0b447f449a20
bwkw <na.shota.1139@gmail.com>
2022-01-16 20:57:05 +0900

    [change]js以下のファイルをjsからパス指定出来る様に変更

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,16 @@ const mix = require('laravel-mix');
  |
  */
 
+const path = require('path');
+mix.webpackConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve('./resources/js'),
+    }
+  }
+});
+
+
 mix.js('resources/js/app.js', 'public/js')
     .react()
     .sass('resources/sass/app.scss', 'public/css');
