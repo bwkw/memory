38f6a76fdf29963a8081059615c7896326877557
bwkw <na.shota.1139@gmail.com>
2022-01-20 22:33:07 +0900

    [update]materiai-uiのGridの利用

--- resources/js/components/Card/BaseCard.js
+++ resources/js/components/Card/BaseCard.js
@@ -5,18 +5,19 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
 
 
 export default function BaseCard(props) {
   return (
-    <div>
+    <Grid container direction="row" spacing={4} columns={{ md: 11 }} justifyContent="center" alignItems="center">
       {props.items.map((item) => (
-        <div key={item.id}>
-          <Card sx={{ maxWidth: 345 }}>
+        <Grid item xs={8} md={3}>
+          <Card>
             <CardMedia
               component="img"
               height="200"
+              width="300"
               image={item.image_path}
               alt={item.name}
             />
@@ -33,8 +34,8 @@ export default function BaseCard(props) {
               <Button size="small" href={`/travels/${item.id}`}>Details</Button>
             </CardActions>
           </Card>
-        </div>
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
 }
