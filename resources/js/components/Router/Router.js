706440fd498e7218e4d34669f9ab57dd290b5fe3
bwkw <na.shota.1139@gmail.com>
2022-01-22 23:02:23 +0900

    [add]header追加

--- resources/js/components/Router/Router.js
+++ resources/js/components/Router/Router.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import CategoryIndex from '@/components/Category/Index';
 import CategoryShow from '@/components/Category/Show';
 import HomeIndex from '@/components/Home/Index';
+import HeaderBar from '@/components/Bar/HeaderBar';
+import { theme } from '@/theme'
+import { ThemeProvider } from '@mui/material/styles';
+
 
 const Router = () => {
+  const Body = (
+    <Routes>
+      <Route path="/" element={<HomeIndex />} />
+      <Route path="/travels" element={<CategoryIndex category="travels" />} />
+      <Route path="/travels/:id" element={<CategoryShow category="travels" />} />
+      <Route path="/foods" element={<CategoryIndex category="foods" />} />
+      <Route path="/sceneries" element={<CategoryIndex category="sceneries" />} />
+      <Route path="/datings" element={<CategoryIndex category="datings" />} />
+    </Routes>
+  )
+  
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeIndex />} />
-        <Route path="/travels" element={<CategoryIndex category="travels" />} />
-        <Route path="/travels/:id" element={<CategoryShow />} />
-        <Route path="/foods" element={<CategoryIndex category="foods" />} />
-        <Route path="/sceneries" element={<CategoryIndex category="sceneries" />} />
-        <Route path="/datings" element={<CategoryIndex category="datings" />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <HeaderBar />
+        { Body }
+      </ThemeProvider>
     </BrowserRouter>
   )
 }
