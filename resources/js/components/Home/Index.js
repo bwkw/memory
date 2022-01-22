022fb0c85e7e46c07dfeed5e8cef11be48587cf7
bwkw <na.shota.1139@gmail.com>
2022-01-22 19:14:38 +0900

    [clean]コード整理

--- resources/js/components/Home/Index.js
+++ resources/js/components/Home/Index.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Category from '../Category/Category';
+
+import Category from '@/components/Category/Category';
 import { theme } from '@/theme'
 import { ThemeProvider } from '@mui/material/styles';
 
 
-function Index() {
+{/* HomeのIndexメインコンポーネント */}
+export default function Index() {
   return (
     <ThemeProvider theme={theme}>
       <Category />
     </ThemeProvider>
-  );
-}
-
-
-if (document.getElementById('home_index')) {
-  ReactDOM.render(
-    <Index />, document.getElementById('home_index')
   );
 }
