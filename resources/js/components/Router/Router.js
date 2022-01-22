5779d08e19a3c3c677e22ca260605f5ee65e0446
bwkw <na.shota.1139@gmail.com>
2022-01-22 19:15:25 +0900

    [add]showページ用のrouting追加

--- resources/js/components/Router/Router.js
+++ resources/js/components/Router/Router.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import CategoryIndex from '@/components/Category/Index';
+import CategoryShow from '@/components/Category/Show';
+import HomeIndex from '@/components/Home/Index';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomeIndex />} />
         <Route path="/travels" element={<CategoryIndex category="travels" />} />
+        <Route path="/travels/:id" element={<CategoryShow />} />
         <Route path="/foods" element={<CategoryIndex category="foods" />} />
         <Route path="/sceneries" element={<CategoryIndex category="sceneries" />} />
         <Route path="/datings" element={<CategoryIndex category="datings" />} />
@@ -16,8 +21,8 @@ const Router = () => {
   )
 }
 
-if (document.getElementById('category_index')) {
+if (document.getElementById('main')) {
   ReactDOM.render(
-    <Router />, document.getElementById('category_index')
+    <Router />, document.getElementById('main')
   );
 }
