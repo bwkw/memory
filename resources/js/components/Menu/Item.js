bd55592cbb4d542323514418aa12caa167f7fe21
bwkw <na.shota.1139@gmail.com>
2022-01-28 16:07:15 +0900

    [update]Item.jsのフォント/アイコンを修正

--- resources/js/components/Menu/Item.js
+++ resources/js/components/Menu/Item.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import AirplanemodeActiveTwoToneIcon from '@mui/icons-material/AirplanemodeActiveTwoTone';
+import EscalatorWarningTwoToneIcon from '@mui/icons-material/EscalatorWarningTwoTone';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import PhotoCameraTwoToneIcon from '@mui/icons-material/PhotoCameraTwoTone';
+import RestaurantTwoToneIcon from '@mui/icons-material/RestaurantTwoTone';
+import Typography from '@mui/material/Typography';
 
 
 export default function Item() {
+  
   const menus = [
-    {"menu": "Travel一覧", "url": "/travels"},
-    {"menu": "Food一覧", "url": "/foods"},
-    {"menu": "Scenery一覧", "url": "/sceneries"},
-    {"menu": "Dating一覧", "url": "/datings"},
+    {"icon":<AirplanemodeActiveTwoToneIcon />, "menu": "Travel 一覧", "url": "/travels"},
+    {"icon":<RestaurantTwoToneIcon />, "menu": "Food 一覧", "url": "/foods"},
+    {"icon":<PhotoCameraTwoToneIcon />, "menu": "Scenery 一覧", "url": "/sceneries"},
+    {"icon":<EscalatorWarningTwoToneIcon />, "menu": "Dating 一覧", "url": "/datings"},
   ];
   
   return (
     <List>
       { menus.map((menu) => (
-        <Link to={ menu.url } style={{ color: 'black' }}>
+        <Link to={ menu.url }>
           <ListItem button key={ menu.menu }>
-            <ListItemText primary={ menu.menu } sx={{ pl: 10 }}/>
+            <ListItemIcon>
+              { menu.icon }
+            </ListItemIcon>
+            <ListItemText primary={<Typography style={{ color: 'black' }}>{ menu.menu }</Typography>} />
           </ListItem>
         </Link>
       ))}
