f49c09459d409b05d1ee0a3cc7a1745fcc4df564
bwkw <na.shota.1139@gmail.com>
2022-01-29 16:05:24 +0900

    [update]Formの入力項目用コンポーネントのテスト版実装

--- resources/js/components/Form/Basic.js
+++ resources/js/components/Form/Basic.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
+import Box from '@mui/material/Box';
 import { Grid } from '@material-ui/core';
 import { useForm, Controller } from "react-hook-form";
 import TextField from "@material-ui/core/TextField";
 import { Button, MenuItem } from "@material-ui/core";
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import { UserInputData } from "@/components/Form/AllForm";
+import TableContainer from '@material-ui/core/TableContainer';
+import Paper from '@material-ui/core/Paper';
 
 
 export default function Basic(props) {
@@ -16,67 +20,80 @@ export default function Basic(props) {
       pullDown: "",
     },
   });
-  
-  const onSubmit = (action) => {
-    if (action === 'next') {
-      props.handleNext();
-    }
+  const { currentState, setCurrentState } = useContext(UserInputData);
+  const onSubmit = (data) => {
+    props.handleNext();
+    setCurrentState({...currentState, "Basic": data });
   };
   
   return (
-    <Grid container>
-      <Grid sm={2}/>
-      <Grid lg={8} sm={8} spacing={10}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Controller                      
-            control={control}
-            name="checkBox"
-            render={({ field: { value, onChange } }) => (
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={value}
-                    onChange={onChange}
-                    color='primary'
-                  />
-                }
-                label="チェックボックス"
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="textBox"
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="テキストフィールド"
-                fullWidth
-                margin="normal"
-                placeholder="プレースホルダー"
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="pullDown"
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="プルダウンリスト"
-                fullWidth
-                margin="normal"
-                id="select"
-                select
+    <TableContainer component={Paper}>
+      <Grid container>
+        <Grid sm={2} />
+        <Grid lg={8} sm={8} spacing={10}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Controller                      
+              control={control}
+              name="checkBox"
+              render={({ field: { value, onChange } }) => (
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={value}
+                      onChange={onChange}
+                      color='primary'
+                    />
+                  }
+                  label="チェックボックス"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="textBox"
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="テキストフィールド"
+                  fullWidth
+                  margin="normal"
+                  placeholder="プレースホルダー"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="pullDown"
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="プルダウンリスト"
+                  fullWidth
+                  margin="normal"
+                  id="select"
+                  select
+                >
+                  <MenuItem value="one">選択肢1</MenuItem>
+                  <MenuItem value="two">選択肢2</MenuItem>
+                  <MenuItem value="three">選択肢3</MenuItem>
+                </TextField>
+              )}
+            />
+            <Box
+              mt={2}
+              mb={4}
+            >
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
               >
-                <MenuItem value="one">選択肢1</MenuItem>
-                <MenuItem value="two">選択肢2</MenuItem>
-                <MenuItem value="three">選択肢3</MenuItem>
-              </TextField>
-            )}
-          />
-        </form>
+                次へ
+              </Button>
+            </Box>
+          </form>
+        </Grid>
       </Grid>
-    </Grid>
+    </TableContainer>
   );
 }
