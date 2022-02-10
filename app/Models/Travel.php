cc6eb89d25d8d81ff7d16c9cbf89ef512ae4a1aa
bwkw <na.shota.1139@gmail.com>
2022-02-10 17:18:29 +0900

    [add]Travel.phpにguard追加

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Travel extends Model
 {
     use HasFactory;
+    
+    protected $guarded = [
+        'id',
+        'image_path',
+        'created_at',
+        'updated_at'
+    ];
 }
