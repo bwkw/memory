e3c9715e168f81ceda4b73e2e9943f6493396168
bwkw <na.shota.1139@gmail.com>
2022-02-10 17:20:20 +0900

    [update]image_pathカラムをnullableに

--- database/migrations/2022_01_04_120735_create_travel_table.php
+++ database/migrations/2022_01_04_120735_create_travel_table.php
@@ -19,7 +19,7 @@ class CreateTravelTable extends Migration
             $table->float('latitude', 10, 7);
             $table->float('longitude', 10, 7);
             $table->datetime('shooting_date');
-            $table->string('image_path', 300);
+            $table->string('image_path', 300)->nullable();
             $table->timestamps();
         });
     }
