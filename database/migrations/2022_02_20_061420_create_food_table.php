07c9d0bdab577fe80ffb516cfa91032a10657046
bwkw <na.shota.1139@gmail.com>
2022-02-20 15:32:50 +0900

    [add] FoodModelに関するファイル追加

---
+++ database/migrations/2022_02_20_061420_create_food_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('food', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->float('latitude', 10, 7);
+            $table->float('longitude', 10, 7);
+            $table->datetime('shooting_date');
+            $table->string('image_path', 300);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('food');
+    }
+}
