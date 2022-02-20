00485f788e354d181855704ac10745486f361635
bwkw <na.shota.1139@gmail.com>
2022-02-20 16:39:22 +0900

    [add] 削除ボタン/処理実装

--- resources/js/components/Card/BaseCard.js
+++ resources/js/components/Card/BaseCard.js
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
+import { Button } from "@material-ui/core";
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -9,21 +11,34 @@ import Typography from '@mui/material/Typography';
 
 
 export default function BaseCard(props) {
+  
+  const onDelete = (id) => {
+    axios
+      .post(`/api/travels/${id}/delete`)
+      .then(response => {
+        props.setDatas(response.data);
+      })
+      .catch(() => {
+        console.log('通信に失敗しました');
+      });
+	  };
+  
+  
   return (
     <Grid container direction="row" spacing={4} columns={{ md: 11 }} justifyContent="center" alignItems="center">
-      {props.items.map((item) => (
-        <Grid key={item.id} item xs={8} md={3}>
+      {props.datas.map((data) => (
+        <Grid key={data.id} item xs={8} md={3}>
           <Card>
             <CardMedia
               component="img"
               height="200"
               width="300"
-              image={item.image_path}
-              alt={item.name}
+              image={data.image_path}
+              alt={data.name}
             />
             <CardContent>
               <Typography gutterBottom variant="h6" component="div">
-                {item.name}
+                {data.name}
               </Typography>
               <Typography variant="body2" color="text.secondary">
                 Lizards are a widespread group of squamate reptiles, with over 6,000
@@ -31,9 +46,42 @@ export default function BaseCard(props) {
               </Typography>
             </CardContent>
             <CardActions>
-              <Link to={ `/travels/${item.id}` }>
-                Details
-              </Link>
+              <Grid
+                container
+                columns={{ xs: 12 }}
+                spacing={3}
+                direction="row"
+                alignItems="center"
+              >
+                <Grid item xs={0} md={1}/>
+                <Grid item xs={1}>
+                  <Link to={`/travels/${data.id}`}>
+                    Details
+                  </Link>
+                </Grid>
+                <Grid item xs={1} md={2} lg={3}/>
+                <Grid item xs={2}>
+                  <Button 
+                    variant="contained" 
+                    color="primary" 
+                    style={{ fontFamily:['Moon Dance', 'Noto Serif JP'] }}
+                    onClick={() => onDelete(data.id)}
+                  >
+                    Delete
+                  </Button>
+                </Grid>
+                <Grid item xs={1} lg={2}/>
+                <Grid item xs={2}>
+                  <Button 
+                    variant="contained" 
+                    color="secondary" 
+                    style={{ fontFamily:['Moon Dance', 'Noto Serif JP'] }}
+                  >
+                    Update
+                  </Button>
+                </Grid>
+                <Grid item xs={1} />
+              </Grid>
             </CardActions>
           </Card>
         </Grid>
