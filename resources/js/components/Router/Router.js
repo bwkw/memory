a98fb0ae32e12b4771f3d0b0072c6a54e6bd6759
bwkw <na.shota.1139@gmail.com>
2022-02-20 15:30:45 +0900

    [add] 各CategoryのRouting追加

--- resources/js/components/Router/Router.js
+++ resources/js/components/Router/Router.js
@@ -18,10 +18,17 @@ function Router() {
       <Route path="/travels/create" element={<CategoryCreate category="travels" />} />
       <Route path="/travels/:id" element={<CategoryShow category="travels" />} />
       <Route path="/foods" element={<CategoryIndex category="foods" />} />
+      <Route path="/foods/create" element={<CategoryCreate category="foods" />} />
+      <Route path="/foods/:id" element={<CategoryShow category="foods" />} />
       <Route path="/sceneries" element={<CategoryIndex category="sceneries" />} />
+      <Route path="/sceneries/create" element={<CategoryCreate category="sceneries" />} />
+      <Route path="/sceneries/:id" element={<CategoryShow category="sceneries" />} />
       <Route path="/datings" element={<CategoryIndex category="datings" />} />
+      <Route path="/datings/create" element={<CategoryCreate category="datings" />} />
+      <Route path="/datings/:id" element={<CategoryShow category="datings" />} />
     </Routes>
   );
+  
   
   return (
     <BrowserRouter>
