e5fde47c9a30c6ab8a8b00eb699e87d20fbcdcdb
bwkw <na.shota.1139@gmail.com>
2022-03-02 14:40:34 +0900

    [update] api関連routingをapi.phpからweb.phpに

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,5 @@
 <?php
 
-use App\Http\Controllers\ApiDatingController;
-use App\Http\Controllers\ApiFoodController;
-use App\Http\Controllers\ApiSceneryController;
-use App\Http\Controllers\ApiScheduleController;
-use App\Http\Controllers\ApiTravelController;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,43 +13,3 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::group(['prefix' => 'datings', 'middleware' => 'api'], function(){
-    Route::get('/', [ApiDatingController::class, 'index'])->name('api_dating_index');
-    Route::post('/', [ApiDatingController::class, 'store'])->name('api_dating_store');
-    Route::get('/{dating}', [ApiDatingController::class, 'show'])->name('api_dating_show');
-    Route::post('/{dating}/delete', [ApiDatingController::class, 'destroy'])->name('api_dating_delete');
-    Route::post('/{dating}/edit', [ApiDatingController::class, 'update'])->name('api_dating_update');
-});
-
-Route::group(['prefix' => 'foods', 'middleware' => 'api'], function(){
-    Route::get('/', [ApiFoodController::class, 'index'])->name('api_food_index');
-    Route::post('/', [ApiFoodController::class, 'store'])->name('api_food_store');
-    Route::get('/{food}', [ApiFoodController::class, 'show'])->name('api_food_show');
-    Route::post('/{food}/delete', [ApiFoodController::class, 'destroy'])->name('api_food_delete');
-    Route::post('/{food}/edit', [ApiFoodController::class, 'update'])->name('api_food_update');
-});
-
-Route::group(['prefix' => 'sceneries', 'middleware' => 'api'], function(){
-    Route::get('/', [ApiSceneryController::class, 'index'])->name('api_scenery_index');
-    Route::post('/', [ApiSceneryController::class, 'store'])->name('api_scenery_store');
-    Route::get('/{scenery}', [ApiSceneryController::class, 'show'])->name('api_scenery_show');
-    Route::post('/{scenery}/delete', [ApiSceneryController::class, 'destroy'])->name('api_scenery_delete');
-    Route::post('/{scenery}/edit', [ApiSceneryController::class, 'update'])->name('api_scenery_update');
-});
-
-Route::group(['prefix' => 'travels', 'middleware' => 'api'], function(){
-    Route::get('/', [ApiTravelController::class, 'index'])->name('api_travel_index');
-    Route::post('/', [ApiTravelController::class, 'store'])->name('api_travel_store');
-    Route::get('/{travel}', [ApiTravelController::class, 'show'])->name('api_travel_show');
-    Route::post('/{travel}/delete', [ApiTravelController::class, 'destroy'])->name('api_travel_delete');
-    Route::post('/{travel}/edit', [ApiTravelController::class, 'update'])->name('api_travel_update');
-});
-
-Route::group(['prefix' => 'schedules', 'middleware' => 'api'], function(){
-    Route::get('/', [ApiScheduleController::class, 'index'])->name('api_schedule_index');
-    Route::post('/', [ApiScheduleController::class, 'store'])->name('api_schedule_store');
-    // Route::get('/{travel}', [ApiTravelController::class, 'show'])->name('api_travel_show');
-    // Route::post('/{travel}/delete', [ApiTravelController::class, 'destroy'])->name('api_travel_delete');
-    // Route::post('/{travel}/edit', [ApiTravelController::class, 'update'])->name('api_travel_update');
-});
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\TravelController;
+use App\Http\Controllers\ApiDatingController;
+use App\Http\Controllers\ApiFoodController;
+use App\Http\Controllers\ApiSceneryController;
+use App\Http\Controllers\ApiScheduleController;
+use App\Http\Controllers\ApiTravelController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +21,47 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-// Route::get('/', [HomeController::class, 'index'])->name('index');
-// Route::get('/travels', [TravelController::class, 'index'])->name('travel_index');
+Route::group(['prefix' => 'api/datings'], function(){
+    Route::get('/', [ApiDatingController::class, 'index'])->name('api_dating_index');
+    Route::post('/', [ApiDatingController::class, 'store'])->name('api_dating_store');
+    Route::get('/{dating}', [ApiDatingController::class, 'show'])->name('api_dating_show');
+    Route::post('/{dating}/delete', [ApiDatingController::class, 'destroy'])->name('api_dating_delete');
+    Route::post('/{dating}/edit', [ApiDatingController::class, 'update'])->name('api_dating_update');
+});
+
+Route::group(['prefix' => 'api/foods'], function(){
+    Route::get('/', [ApiFoodController::class, 'index'])->name('api_food_index');
+    Route::post('/', [ApiFoodController::class, 'store'])->name('api_food_store');
+    Route::get('/{food}', [ApiFoodController::class, 'show'])->name('api_food_show');
+    Route::post('/{food}/delete', [ApiFoodController::class, 'destroy'])->name('api_food_delete');
+    Route::post('/{food}/edit', [ApiFoodController::class, 'update'])->name('api_food_update');
+});
+
+Route::group(['prefix' => 'api/sceneries'], function(){
+    Route::get('/', [ApiSceneryController::class, 'index'])->name('api_scenery_index');
+    Route::post('/', [ApiSceneryController::class, 'store'])->name('api_scenery_store');
+    Route::get('/{scenery}', [ApiSceneryController::class, 'show'])->name('api_scenery_show');
+    Route::post('/{scenery}/delete', [ApiSceneryController::class, 'destroy'])->name('api_scenery_delete');
+    Route::post('/{scenery}/edit', [ApiSceneryController::class, 'update'])->name('api_scenery_update');
+});
+
+Route::group(['prefix' => 'api/travels'], function(){
+    Route::get('/', [ApiTravelController::class, 'index'])->name('api_travel_index');
+    Route::post('/', [ApiTravelController::class, 'store'])->name('api_travel_store');
+    Route::get('/{travel}', [ApiTravelController::class, 'show'])->name('api_travel_show');
+    Route::post('/{travel}/delete', [ApiTravelController::class, 'destroy'])->name('api_travel_delete');
+    Route::post('/{travel}/edit', [ApiTravelController::class, 'update'])->name('api_travel_update');
+});
+
+Route::group(['prefix' => 'api/schedules', 'middleware' => 'api'], function(){
+    Route::get('/', [ApiScheduleController::class, 'index'])->name('api_schedule_index');
+    Route::post('/', [ApiScheduleController::class, 'store'])->name('api_schedule_store');
+    // Route::get('/{travel}', [ApiTravelController::class, 'show'])->name('api_travel_show');
+    // Route::post('/{travel}/delete', [ApiTravelController::class, 'destroy'])->name('api_travel_delete');
+    // Route::post('/{travel}/edit', [ApiTravelController::class, 'update'])->name('api_travel_update');
+});
+
 
 Route::get('/{any}', function () {
     return view('main');
-})->where('any','.*');
+})->where('any','.*')->middleware('auth');
