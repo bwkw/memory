f62614a3381e0560296435d18e1f210c16577dc0
bwkw <na.shota.1139@gmail.com>
2022-03-04 14:34:38 +0900

    [update] ログイン認証用初期実装

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
